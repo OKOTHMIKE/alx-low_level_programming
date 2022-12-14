ef20c0fdfffc59520f12396ede0f49b228573a5b
OKOTHMIKE <michaelokoth90@gmail.com>
2022-12-14 09:14:42 -0800

    task1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * print alphabet - prints the alpha in lowercase
+ */
+main(void) -  print_alphabet(void)
+{
+	char la;
+
+	for (la = 'a'; la <= 'z'; la++)
+		_putchar(la);
+	_putchar('\n');
+}
