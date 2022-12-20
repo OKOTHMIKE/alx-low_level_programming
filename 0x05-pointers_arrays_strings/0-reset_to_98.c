c2a29558170d40ed48ddce249e74dc5aa5c88c6d
OKOTHMIKE <michaelokoth90@gmail.com>
2022-12-20 00:30:44 -0800

    task0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - function that takes pointer to an integer parameter
+ * @n: integer pointer
+ * Return: 0 success
+ */
+void rest_to_98(int *n)
+{
+	*n = 98;
+}
+
